d9db5710b81eb610c1b0ff2503d7b69f1955e481
Ben Johnston <admiralackbar411@gmail.com>
2024-06-19 15:24:01 +0100

    Initial commit with base for project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="header-area">
+        <h2>ToDone</h2>
+        <p>For all you note taking needs</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
